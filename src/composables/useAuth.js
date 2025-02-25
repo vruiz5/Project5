1d4f31aa19e163520d0780c1d46b62c99e208545
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-02-24 22:33:07 -0600

    Created useAuth

---
+++ src/composables/useAuth.js
@@ -0,0 +1,27 @@
+//import { useRouter } from 'vue-router'
+import router from '../router'
+
+import { firebaseApp } from './composables/useFirebase'
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { useAuth as firebaseAuth } from '@vueuse/firebase/useAuth'
+
+const auth = getAuth(firebaseApp);
+
+const { isAuthenticated, user } = firebaseAuth(auth)
+
+export const useAuth = () => {
+    const router = useRouter()
+
+    const login = async (username, password) => {
+        await signInWithEmailAndPassword(auth, username, password)
+        return isAuthenticated.value
+    }
+    
+    const logout = async () => {
+        await signOut(auth)
+        router.push({ name: 'Home' })
+    }
+    
+    return { isAuthenticated, user, login, logout }
+
+}
