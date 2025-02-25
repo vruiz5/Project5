6474b6d74f36547f7bb76abc3612ed1fec80644e
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-02-24 22:26:17 -0600

    Installed Firebase

---
+++ src/composables/useFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxsGbuxCm5M4kh0cGzKjZFn-r5fABGl-g",
+  authDomain: "game-directory-439bc.firebaseapp.com",
+  projectId: "game-directory-439bc",
+  storageBucket: "game-directory-439bc.firebasestorage.app",
+  messagingSenderId: "799572148951",
+  appId: "1:799572148951:web:659eaf5ff9d50dd0c82692"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
